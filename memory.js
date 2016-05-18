97a5055cb85035f90aff0b27460ce94586427c4d
Cody Barber <cody.n.barber@gmail.com>
2016-05-18 16:44:22 -0400

    Added winner detection, restart button, and almost completed tile flip animations.

--- memory.js
+++ memory.js
@@ -2,6 +2,8 @@ var state = 'first';
 
 var source1;
 var source2;
+var solved = 0;
+
 
 $(document).ready(function() {
 
@@ -17,6 +19,7 @@ $(document).ready(function() {
       source2 = $(this).find('img').attr('src');
       state = 'first';
       if (source1 === source2) {
+        solved += 2;
         $('.open').addClass('solved');
         $('.solved').removeClass('open');
         // $('.solved').removeClass('animated rubberBand');
@@ -28,8 +31,12 @@ $(document).ready(function() {
         }, 1000);
       }
     }
+    var solved = $('.solved').length;
+    var totalCards = $('.tile').length;
+    if (solved === totalCards) {
+      $('#message').text('You Win!!!').addClass('animated infinite flash');
+    }
   });
-
   $('.button').click(function() {
     location.reload();
   })
