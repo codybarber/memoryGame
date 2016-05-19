cde9d970a3b4f1cc12ebe47c8859d92e6e591247
Cody Barber <cody.n.barber@gmail.com>
2016-05-19 15:38:36 -0400

    Added all 16 pics to the game and added shuffling functionality.

--- memory.js
+++ memory.js
@@ -4,8 +4,38 @@ var source1;
 var source2;
 var solved = 0;
 
-
 $(document).ready(function() {
+debugger
+  var pictures = _.shuffle([
+    'monsters-01.png',
+    'monsters-02.png',
+    'monsters-03.png',
+    'monsters-04.png',
+    'monsters-05.png',
+    'monsters-06.png',
+    'monsters-07.png',
+    'monsters-08.png',
+    'monsters-09.png',
+    'monsters-10.png',
+    'monsters-11.png',
+    'monsters-12.png',
+    'monsters-13.png',
+    'monsters-14.png',
+    'monsters-15.png',
+    'monsters-16.png'
+  ]);
+
+  var fourPics = [];
+  fourPics = pictures.slice(0, 4)
+
+  var eightPics = [];
+  eightPics = fourPics.concat(fourPics);
+
+  shuffledPics = _.shuffle(eightPics);
+
+  for (var i = 0; i < 8; i++) {
+    $('#grid').append('<div class="tile">' + '<img class="monster" src="images/' + shuffledPics[i] + '">' + '<div class="back"></div>' + '</div>');
+  }
 
   $('.tile').click(function() {
     if (state === 'first') {
